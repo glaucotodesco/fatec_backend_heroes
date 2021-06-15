25b93c55a84b6c7530a66ae098bd2903434801a7
Glauco Todesco <glauco.todesco@gmail.com>
2021-06-15 11:01:02 -0300

    Crud finished

--- src/main/java/com/heroes/controller/HeroController.java
+++ src/main/java/com/heroes/controller/HeroController.java
@@ -8,17 +8,20 @@ import com.heroes.entities.Hero;
 import com.heroes.repository.HeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/heroes")           // 
+@CrossOrigin
 public class HeroController {
 
     @Autowired
@@ -47,19 +50,32 @@ public class HeroController {
         return hero;
     }
 
+    //EndPoint 3
+    //Salva um heroi, é necessário passar os dados do heroi no formato JSON
     @PostMapping
-    public void salvar(){
-
+    public Hero salvar(@RequestBody Hero hero){
+        hero = repo.save(hero);
+        return hero;
     }
 
-    @PutMapping
-    public void alterar(){
-        
+     //EndPoint 4
+    //Altera um heroi, é necessário passar os dados do heroi no formato JSON e tambem o id
+    @PutMapping("{id}")
+    public Hero alterar(@RequestBody Hero updateHero, @PathVariable Long id){
+        Optional<Hero> op =  repo.findById(id);
+        Hero hero = op.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        hero.setName(updateHero.getName());
+        repo.save(hero);
+        return hero;
     }
 
-    @DeleteMapping
-    public void remover(){
-        
+    //EndPoint 5
+    //Remove um heroi, é necessário passar o id
+    @DeleteMapping("{id}")
+    public void remover(@PathVariable Long id){
+        Optional<Hero> op =  repo.findById(id);
+        Hero hero = op.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        repo.delete(hero);
     }
 
 
